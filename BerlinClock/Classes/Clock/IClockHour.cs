2b6442f647d4e8c2da8a772499fba47f29fe448b
Eduardo Julião <eduardo.juliao@8bitscoffee.com.br>
2017-11-24 13:34:17 -0200

    Re added the projects

---
+++ BerlinClock/Classes/Clock/IClockHour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BerlinClock.Classes
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IClockHour
+    {
+        int CurrentValue { get; }
+        int MaxValue { get; }
+        char Color { get; }
+    }
+}
---
+++ BerlinClockConsole/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using BerlinClock;
+
+namespace BerlinClockConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Continuously();
+        }
+
+        /// <summary>
+        /// Feature test case
+        /// </summary>
+        static void RunCases()
+        {
+            Console.WriteLine(new TimeConverter().convertTime("00:00:00"));
+            Console.WriteLine(new TimeConverter().convertTime("13:17:01"));
+            Console.WriteLine(new TimeConverter().convertTime("23:59:59"));
+            Console.WriteLine(new TimeConverter().convertTime("24:00:00"));
+        }
+
+        /// <summary>
+        /// Prompt user to insert a time
+        /// </summary>
+        static void RunAsk()
+        {
+            Console.Write("Please, inform a time: ");
+            var time = Console.ReadLine();
+
+            var berlinTime = new TimeConverter().convertTime(time);
+
+            Console.WriteLine(berlinTime);
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Runs Continuously
+        /// </summary>
+        static void Continuously()
+        {
+            while (true)
+            {
+                var berlinTime = new TimeConverter().convertTime(DateTime.Now.ToString("HH:mm:ss"));
+                Console.WriteLine(berlinTime);
+                Thread.Sleep(999);
+                Console.Clear();
+            }
+        }
+    }
+}
